29eca419b79b520485febc9c652de2ceaf3437c6
Niels Grewe <thebeing+gna@halbordnung.de>
2013-06-19 02:37:20 +0000

    Improve table views, fix for missing Unicode support on iOS 5.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/webservices/trunk@36729 72102866-910b-0410-8b05-ffd578937521

---
+++ iOSWebServices/iOSWebServices.h
@@ -0,0 +1,13 @@
+//
+//  iOSWebServices.h
+//  iOSWebServices
+//
+//  Created by Niels Grewe on 18.05.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSWebServices : NSObject
+
+@end
---
+++ iOSWebServices/iOSWebServices.m
@@ -0,0 +1,13 @@
+//
+//  iOSWebServices.m
+//  iOSWebServices
+//
+//  Created by Niels Grewe on 18.05.13.
+//
+//
+
+#import "iOSWebServices.h"
+
+@implementation iOSWebServices
+
+@end
